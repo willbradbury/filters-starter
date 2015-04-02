d9bcb95974b16d8f40bf141a01dc0993c6f6c70f
Will Bradbury <willbradbury@gmail.com>
2015-04-01 20:30:28 -0700

    Finished project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
+var BRIGHTNESS_SHIFT = 20;
+var THRESHOLD = 150;
+
+function applyFilter(pixels,modifyFunc){
+	for(var i= 0;i<pixels.length;i+=4){
+		var red = pixels[i];
+		var green = pixels[i+1];
+		var blue = pixels[i+2];
+		
+		var newPixels = modifyFunc(red,green,blue)
+		
+		pixels[i] = newPixels[0];
+		pixels[i+1] = newPixels[1];
+		pixels[i+2] = newPixels[2];
+	}
+	
+	return pixels;
+}
+
 /* Filters the given pixels to grayscale.
  *
  * Arguments:
  * pixels -- an array of pixel values
  */
 function filterGrayscale(pixels) {
+	return applyFilter(pixels,function(red,green,blue){
+		var avg = red*.3+green*.4+blue*.3;
+		return [avg,avg,avg];
+	});
 }
 
 /* Brightens the given pixels.
@@ -12,6 +35,9 @@ function filterGrayscale(pixels) {
  * pixels -- an array of pixel values
  */
 function filterBrighten(pixels) {
+	return applyFilter(pixels,function(red,green,blue){
+		return [red + BRIGHTNESS_SHIFT, green + BRIGHTNESS_SHIFT, blue + BRIGHTNESS_SHIFT]
+	});
 }
 
 /* Applies a threshold filter to the given pixels. Makes all pixels above
@@ -21,4 +47,11 @@ function filterBrighten(pixels) {
  * pixels -- an array of pixel values
  */
 function filterThreshold(pixels) {
+	return applyFilter(pixels,function(red,green,blue){
+		if(Math.sqrt(red*red + green*green + blue*blue) > THRESHOLD){
+			return [255,255,255];
+		}else{
+			return [0,0,0];
+		}
+	});
 }
